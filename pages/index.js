8829645e5490ad5993b3e0add8b1199e586cdd28
windsBro <360117108@qq.com>
2020-10-15 10:40:41 +0800

    init

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import '../asset/css/common.less'
+
+export default () => <div>
+    <style>{
+        `
+        .red{
+            color: red;
+        }
+        `
+        }
+    </style>
+    <div className="red">
+        hello world
+    </div>
+    <div className="blueviolet">
+        ni hao ,dear
+    </div>
+    </div>
